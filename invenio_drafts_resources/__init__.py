41abdf18cf35ac9624fc18a087607a0a77f7a6e2
Christoph Ladurner <christoph.ladurner@tugraz.at>
2025-06-03 21:53:21 +0200

    release: v7.0.0

--- invenio_drafts_resources/__init__.py
+++ invenio_drafts_resources/__init__.py
@@ -2,7 +2,7 @@
 #
 # Copyright (C) 2020-2025 CERN.
 # Copyright (C) 2020-2023 Northwestern University.
-# Copyright (C) 2024 Graz University of Technology.
+# Copyright (C) 2024-2025 Graz University of Technology.
 #
 # Invenio-Drafts-Resources is free software; you can redistribute it and/or
 # modify it under the terms of the MIT License; see LICENSE file for more
@@ -11,6 +11,6 @@
 """Invenio Drafts Resources module to create REST APIs."""
 
 
-__version__ = "6.2.0"
+__version__ = "7.0.0"
 
 __all__ = ("__version__",)
